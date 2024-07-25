1fbd98d64c4fd219840b479089704e49ce265cec
auro <auro@DESKTOP-AV8I9BT>
2024-07-25 19:15:32 +0300

    Add project files.

---
+++ DoWhileVsWhile/Program.cs
@@ -0,0 +1,24 @@
+﻿//// -------- While ---------
+//Console.WriteLine("Lütfen bir sayı limiti giriniz:");
+//int limit = Convert.ToInt32(Console.ReadLine());
+
+//int sayac = 1;
+
+//while(limit >= sayac)
+//{
+//    Console.WriteLine("Ben bir Patika'lıyım");
+//    sayac++;
+//}
+// -------- Do-While ---------
+
+Console.WriteLine("Lütfen bir sayı limiti giriniz:");
+int limit = Convert.ToInt32(Console.ReadLine());
+
+int sayac = 1;
+do                      
+{
+    Console.WriteLine("Ben bir Patika'lıyım");
+    sayac++;
+}while(limit >= sayac);
+
+//   FARKI DO DÖNGÜSÜ EN AZ BİR KERE ÇALIŞMA GARANTİSİ VERİRKEN WHILE DÖNGÜSÜ BU GARANTİYİ VERMEZ.
